bd39f177853c4c2514ba5b14c506c8b0fbb72e7f
MatheusMatiasUniver <matheusactwind@outlook.com>
2025-06-11 00:49:55 -0300

    commit inicial

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,8 @@
+let quantidadeNumeros = document.getElementById("quantidade");
+let numeroInicial = document.getElementById("de");
+let numeroFinal = document.getElementById("ate");
+
+
+function sortear() {
+console.log(quantidadeNumeros.value);
+}
